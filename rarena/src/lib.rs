0a4a65cd6081dfc2c413a3ac00c7f6fb066b6e77
Al Liu <scygliu1@gmail.com>
2024-06-14 21:05:37 +0800

    Add README

--- rarena/src/lib.rs
+++ rarena/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../../README.md")]
+#![doc = include_str!("../README.md")]
 #![cfg_attr(not(any(feature = "std", test)), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(docsrs, allow(unused_attributes))]
